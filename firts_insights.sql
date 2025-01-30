eaadc1d4289119444799cbbf457e086b458be879
AlisonCG1 <alisoncg@outlook.com>
2025-01-30 12:43:18 -0600

    first commit

---
+++ firts_insights.sql
@@ -0,0 +1,13 @@
+SELECT * FROM Friend f 
+
+SELECT * FROM FriendRequest fr 
+
+SELECT * FROM Post p 
+
+SELECT * FROM Notification n 
+
+SELECT * FROM Profile p 
+
+SELECT * FROM USER
+
+SELECT * FROM Comment c 

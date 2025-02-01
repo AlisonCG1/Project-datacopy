032a07a0a14683a5bdf84e5ed351f9d9b933948e
AlisonCG1 <alisoncg@outlook.com>
2025-02-01 13:44:08 -0600

    Exploratory done

--- firts_insights.sql
+++ firts_insights.sql
@@ -26,3 +26,49 @@ GROUP BY user_id
 ORDER BY comments DESC;
 
 -- Friendship Network 
+SELECT sender_id, receiver_id, status
+FROM FriendRequest fr 
+WHERE status = 'accepted'
+GROUP BY sender_id, receiver_id
+ORDER BY sender_id 
+
+--most common time to post
+SELECT TIME(created_at) AS time, COUNT(*) AS count
+FROM Notification n  
+GROUP BY TIME(created_at)
+ORDER BY count DESC
+LIMIT 1; 
+
+-- Engagement Per Post / most popular user
+SELECT user_id, COUNT(TYPE) AS comments
+FROM Notification n 
+WHERE type == 'comment'
+GROUP BY user_id
+ORDER BY comments DESC;
+
+-- Most accepted USER
+SELECT sender_id, COUNT(sender_id) AS sender_count, status
+FROM FriendRequest fr 
+WHERE status = 'accepted'
+GROUP BY status
+ORDER BY sender_count DESC 
+
+-- Most sent friend requests
+SELECT sender_id, COUNT(sender_id) AS sender_count, status
+FROM FriendRequest fr 
+GROUP BY status
+ORDER BY sender_count DESC
+
+--most common time to post
+SELECT TIME(created_at) AS time, COUNT(*) AS count
+FROM Notification n  
+GROUP BY TIME(created_at)
+ORDER BY count DESC
+LIMIT 1; 
+
+-- Friendship Network 
+SELECT user_id, COUNT(*) AS user_count
+FROM Friend f 
+GROUP BY user_id
+ORDER BY user_count DESC; 
+

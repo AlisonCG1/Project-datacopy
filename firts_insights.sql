2d93530ded04c8aeca93cdba5ed48bf174fb290f
AlisonCG1 <alisoncg@outlook.com>
2025-02-01 12:54:20 -0600

    exploratory

--- firts_insights.sql
+++ firts_insights.sql
@@ -11,3 +11,18 @@ SELECT * FROM Profile p
 SELECT * FROM USER
 
 SELECT * FROM Comment c 
+
+-- Most Active users
+SELECT user_id, COUNT(*) AS total_posts 
+FROM post
+GROUP BY user_id 
+ORDER BY total_posts DESC;
+
+-- Engagement Per Post / most popular user
+SELECT user_id, COUNT(TYPE) AS comments
+FROM Notification n 
+WHERE type == 'comment'
+GROUP BY user_id
+ORDER BY comments DESC;
+
+-- Friendship Network 
